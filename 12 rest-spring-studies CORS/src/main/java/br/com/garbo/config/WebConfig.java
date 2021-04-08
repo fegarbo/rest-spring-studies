48fba665f272427250517760d8d9872a3cd5444f
Fernando Garbo <nando.garbo@gmail.com>
2021-04-08 08:41:31 -0300

    Configuring CORS

---
+++ 12 rest-spring-studies CORS/src/main/java/br/com/garbo/config/WebConfig.java
@@ -0,0 +1,66 @@
+package br.com.garbo.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import br.com.garbo.serialization.converter.YamlJackson2HttpMessageConverter;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+	
+	private static final MediaType MEDIA_TYPE_YAML = MediaType.valueOf("application/x-yaml");
+	
+	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(new YamlJackson2HttpMessageConverter());
+	}	
+	
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		
+		//Via Extension: http://localhost:8080/api/person/v1.json
+		/*
+		 * configurer.favorParameter(false) .ignoreAcceptHeader(false)
+		 * .defaultContentType(MediaType.APPLICATION_JSON) .mediaType("json",
+		 * MediaType.APPLICATION_JSON) .mediaType("xml", MediaType.APPLICATION_XML);
+		 */
+		
+		//Via Query Param: http://localhost:8080/api/person/v1?mediaType=xml
+		/*
+		 * configurer.favorPathExtension(false) .favorParameter(true)
+		 * .parameterName("mediaType") .ignoreAcceptHeader(true)
+		 * .useRegisteredExtensionsOnly(false)
+		 * .defaultContentType(MediaType.APPLICATION_JSON) .mediaType("json",
+		 * MediaType.APPLICATION_JSON) .mediaType("xml", MediaType.APPLICATION_XML);
+		 */
+
+		//Via Header: http://localhost:8080/api/person/v1      Ex: Accept = application/xml
+		configurer.favorPathExtension(false)
+			.favorParameter(false)
+			.ignoreAcceptHeader(false)
+			.useRegisteredExtensionsOnly(false)
+			.defaultContentType(MediaType.APPLICATION_JSON)
+			.mediaType("json", MediaType.APPLICATION_JSON)
+			.mediaType("xml", MediaType.APPLICATION_XML)
+			.mediaType("x-yaml", MEDIA_TYPE_YAML);			
+	}
+	
+	
+	//Enable access for all application without CORS problem
+	//Works for basic operations(PUT, DELETE, POST and GET)
+	/*
+	 * public void addCorsMapping(CorsRegistry registry) {
+	 * registry.addMapping("/**"); }
+	 */
+	
+	//This way it works for every method specified
+	public void addCorsMapping(CorsRegistry registry) {
+		registry.addMapping("/**")
+		.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+	}	
+}
---
+++ 12 rest-spring-studies CORS/src/main/java/br/com/garbo/services/PersonServices.java
@@ -0,0 +1,53 @@
+package br.com.garbo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.garbo.converter.DozerConverter;
+import br.com.garbo.data.model.Person;
+import br.com.garbo.data.vo.v1.PersonVO;
+import br.com.garbo.exception.ResourceNotFoundException;
+import br.com.garbo.repository.PersonRepository;
+
+@Service //Spring cuida da injecao de dependencia, desta maneira nao preciso instanciar ou deixar static 
+public class PersonServices {
+	
+	@Autowired
+	PersonRepository repository;
+	
+	public PersonVO create(PersonVO person) {
+		var entity = DozerConverter.parseObject(person, Person.class);
+		return DozerConverter.parseObject(repository.save(entity), PersonVO.class);		
+	}		
+	
+	public PersonVO update(PersonVO person) {
+		var entity = repository.findById(person.getKey())
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setAddress(person.getAddress());
+		entity.setGender(person.getGender());
+		
+		return DozerConverter.parseObject(repository.save(entity), PersonVO.class);
+	}	
+	
+	public PersonVO findById(Long id) {		
+		var entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+		
+		return DozerConverter.parseObject(repository.save(entity), PersonVO.class);
+	}	
+	
+	public List<PersonVO> findAll() {
+
+		return DozerConverter.parseListObjects(repository.findAll(), PersonVO.class);
+	}
+	
+	public void delete(Long id) {
+		Person entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+		repository.delete(entity);
+	}	
+}
